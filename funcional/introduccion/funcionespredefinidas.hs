f8111d21adf872c6ae7e2a54e1eb7044a3cccd77
C-C1209 <71782181+C-C1209@users.noreply.github.com>
2020-11-25 11:39:07 -0700

    Práctica funciones predefinidas en Haskell

---
+++ funcional/introduccion/funcionespredefinidas.hs
@@ -0,0 +1,32 @@
+--f1  abs x "Devuelve el numero en positivo"
+f1 x= abs x 
+--f2  ceiling x "Devuelve un entero no menor al ingresado ejemplo 3.4 o 3.7 retorna 3"
+f2 x= ceiling x
+--f3  succ x "Devuelve el numero que sigue"
+f3 x= succ x
+--f4 min x y "Devuelve el valor menor de los datos ingresados"
+f4 x y= min x y
+--f5  max x y "Devuelve el valor maximo de los datos ingresados"
+f5 x y= max x y
+--f6  div x y "Devuelve la division de los numeros"
+f6 x y= div x y
+--f7  mod x y "Devuelve el modulo de la division"
+f7 x y= mod x y
+--f8  even x "devuelve un valor booleano True si el valor es par"
+f8 x= even x
+--f9  floor x "devuelve el valor mas grande pero no mas grande que el entero ingresado ejemplo 3.7 retorna 3"
+f9 x= floor x
+--f10  gcd x y "devuelve el maximo comun divisor"
+f10 x y= gcd x y
+--f11  not X  "devuelve lo contrario de un booleano"
+f11 x= not x
+--f12  odd x "devuelve un valor booleano True si el valor es impar"
+f12 x= odd x
+--f13  succ x Devuelve el numero que sigue"
+f13 x= succ x
+--f14  signum x "Devuelve 0 si el numero es 0 , devuelve -1 si es negativo, devuelve 1 si el numero es positivo"
+f14 x= signum x
+--f15  snd (x,y) "Devuelve el segundo objeto de una tupla"
+f15 x y= snd (x,y)
+--f16  signum x "Devuelve 0 si el numero es 0 , devuelve -1 si es negativo, devuelve 1 si el numero es positivo"
+f16 x= signum x
